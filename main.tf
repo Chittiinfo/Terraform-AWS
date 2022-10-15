09b1d49d1171882b858d58d62f740f2fc1dcc5c5
Chittiinfo <92975052+Chittiinfo@users.noreply.github.com>
2022-10-15 22:50:05 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,33 +1,23 @@
 #This Terraform Code Deploys Basic VPC Infra.
-provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
-}
-
 terraform {
-  required_version = "<= 0.14" #Forcing which version of Terraform needs to be used
+  backend "s3" {
+    bucket = "chitti7702"
+    key    = "ecommerce.tfstate"
+    region = "ap-south-1"
+  }
   required_providers {
-    aws = {
-      version = "<= 3.0.0" #Forcing which version of plugin needs to be used.
-      source = "hashicorp/aws"
-    }
+      aws = {
+          source = "hashicorp/aws"
+          version = "4.5.0"
+      }
   }
 }
 
-resource "aws_vpc" "default" {
-    cidr_block = "${var.vpc_cidr}"
-    enable_dns_hostnames = true
-    tags = {
-        Name = "${var.vpc_name}"
-	Owner = "Nag S"
-	environment = "${var.environment}"
-    }
-}
 
-resource "aws_internet_gateway" "default" {
-    vpc_id = "${aws_vpc.default.id}"
-	tags = {
-        Name = "${var.IGW_name}"
-    }
+provider "aws" {
+    # profile = "default"
+    # region  = "ap-south-1"
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
 }
--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable aws_access_key {}
 variable aws_secret_key {}
-variable vpc_cidr {}
 variable aws_region {}
-variable aws_access_key {}
-variable aws_access_key {}
-variable aws_access_key {}
+variable vpc_cidr {}
+variable vpc_name {}
+variable environment {}
+variable IGW_name {}
+variable public_subnet1_cidr {}
+variable public_subnet1_name {}
