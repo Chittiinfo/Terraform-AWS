fd1e625001a7407edfb14b9862822f66084faea7
Chittiinfo <92975052+Chittiinfo@users.noreply.github.com>
2022-10-16 15:09:56 +0530

    c

--- s3.tf
+++ s3.tf
@@ -1,8 +1,11 @@
 resource "aws_s3_bucket" "mybucket1" {
-  bucket = "${var.vpc_name}-mybucket1"
+  bucket = "${lower(var.vpc_name)}-mybucket1"
 
   tags = {
-    Name        = "${var.vpc_name}-mybucket1"
+    Name        = "${lower(var.vpc_name)}-mybucket1"
     Environment = "${var.environment}"
   }
+depends_on = [
+    aws_subnet.subnet3-public
+  ]
 }
--- subnets.tf
+++ subnets.tf
@@ -7,6 +7,9 @@ resource "aws_subnet" "subnet1-public" {
     tags = {
         Name = "${var.public_subnet1_name}"
     }
+depends_on = [
+    aws_internet_gateway.default
+  ]
 }
 
 resource "aws_subnet" "subnet2-public" {
@@ -18,6 +21,9 @@ resource "aws_subnet" "subnet2-public" {
     tags = {
         Name = "${var.public_subnet2_name}"
     }
+depends_on = [
+    aws_subnet.subnet1-public
+  ]
 }
 
 resource "aws_subnet" "subnet3-public" {
@@ -29,4 +35,7 @@ resource "aws_subnet" "subnet3-public" {
     tags = {
         Name = "${var.public_subnet3_name}"
     }
+depends_on = [
+    aws_subnet.subnet2-public
+  ]
 }
